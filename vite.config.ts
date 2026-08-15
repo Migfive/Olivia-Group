a61d759d3d6710f3d1792bde6cec6fce9461dc5e
Miguel <miguelangelo23404@gmail.com>
2026-08-14 19:15:48 -0500

    changes in package.json second

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/olivias-group-web/',
+  base: '/Olivia-Group/',
   plugins: [react(), tailwindcss()],
   test: {
     environment: 'jsdom',
